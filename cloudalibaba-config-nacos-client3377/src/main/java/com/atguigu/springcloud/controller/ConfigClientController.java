a6590c7e194b2672197d5f0987d6977a4bc8074a
james <yanmz_vip@163.com>
2020-08-08 21:21:07 +0800

    Spring Cloud nacos 配置中心

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,24 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yanmz
+ * @version 1.0
+ * @date 2020/8/6 15:13
+ */
+@RestController
+@RefreshScope// 支持nacos的动态刷新
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/config/info")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
