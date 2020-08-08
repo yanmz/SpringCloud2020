444fb103f78c78d7192972ff5fa4fa8c836c46c2
james <yanmz_vip@163.com>
2020-08-08 18:42:17 +0800

    Spring Cloud Bus 服务总线，实现全局广播/定点通知

---
+++ cloud-config-client-3366/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,27 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yanmz
+ * @version 1.0
+ * @date 2020/8/5 20:16
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return "server.port = " + serverPort + "\t configInfo=" + configInfo;
+    }
+}
