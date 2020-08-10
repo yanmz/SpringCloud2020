3aa0ce8d9f21de1eb3dbdd74914d3c33a18f0932
james <yanmz_vip@163.com>
2020-08-10 09:50:36 +0800

    spring cloud 服务提供者模块

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/dao/PaymentDao.java
@@ -0,0 +1,28 @@
+package com.atguigu.springcloud.dao;
+
+import com.atguigu.springcloud.entities.Payment;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * @author Tom
+ */
+@Mapper
+public interface PaymentDao {
+
+    /**
+     * 新增
+     *
+     * @param payment
+     * @return
+     */
+    int create(Payment payment);
+
+    /**
+     * 根据Id查询
+     *
+     * @param id
+     * @return
+     */
+    Payment getPaymentById(@Param("id") Long id);
+}
