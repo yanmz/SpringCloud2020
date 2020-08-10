72dbfff2d4bc2e1edeaad65f0c5ebffe376b05f9
james <yanmz_vip@163.com>
2020-08-10 11:05:21 +0800

    spring cloud 与OpenFeign集成 消费者服务

---
+++ cloud-consumer-feign-order80/src/main/java/com/atguigu/springcloud/OrderFeignMain80.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author yanmz
+ * @version 1.0
+ * @date 2020/8/3 17:05
+ */
+@SpringBootApplication
+@EnableFeignClients
+public class OrderFeignMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderFeignMain80.class,args);
+    }
+}
---
+++ cloud-consumer-feign-order80/src/main/java/com/atguigu/springcloud/config/FeignConfig.java
@@ -0,0 +1,25 @@
+package com.atguigu.springcloud.config;
+
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author yanmz
+ * @version 1.0
+ * @date 2020/8/3 21:14
+ */
+@Configuration
+public class FeignConfig {
+
+    /**
+     * feignClient配置日志级别
+     *
+     * @return
+     */
+    @Bean
+    public Logger.Level feignLoggerLevel() {
+        // 请求和响应的头信息,请求和响应的正文及元数据
+        return Logger.Level.FULL;
+    }
+}
